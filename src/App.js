be6df03e0bedcc267ca8cdf147b9c3c371bf2fb8
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-16 10:51:03 +0200

    modify function component App to class component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 
-function App() {
-  return (
-    <div className="App">
-      <Greet name='Bruce' heroName='Batman'>
-        <p>This is children props</p>
-      </Greet>
-      <Greet name='Clark' heroName='Superman'>
-        <button>Action</button>
-      </Greet>
-      <Greet name='Diana' heroName='Wonder Woman' />
-      <Welcome name='Bruce' heroName='Batman' />
-      <Welcome name='Clark' heroName='Superman' />
-      <Welcome name='Diana' heroName='Wonder Woman' />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet name='Bruce' heroName='Batman'>
+          <p>This is children props</p>
+        </Greet>
+        <Greet name='Clark' heroName='Superman'>
+          <button>Action</button>
+        </Greet>
+        <Greet name='Diana' heroName='Wonder Woman' />
+        <Welcome name='Bruce' heroName='Batman' />
+        <Welcome name='Clark' heroName='Superman' />
+        <Welcome name='Diana' heroName='Wonder Woman' />
+      </div>
+    );
+  }
 }
 
 export default App;
