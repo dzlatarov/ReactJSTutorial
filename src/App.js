27552b6a45cb4b64018f3b1338cd9d536466b64a
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-16 10:39:58 +0200

    using props with functional components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import  Greet  from './components/Greet'
+import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet /> */}
+      <Greet name='Bruce' heroName='Batman'>
+        <p>This is children props</p>
+      </Greet>
+      <Greet name='Clark' heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Diana' heroName='Wonder Woman' />
       {/* <Welcome /> */}
-      <Hello />
+      {/* <Hello /> */}
     </div>
   );
 }
