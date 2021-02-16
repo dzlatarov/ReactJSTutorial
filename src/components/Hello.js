c0678e5656433207bfb543a8e68e1a2395dfe7a2
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-16 09:15:26 +0200

    initial commit

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    return React.createElement('div', null, React.createElement('h1', null, 'Hello Danail'))
+} 
+
+// const Hello = () =>
+//     <div>
+//         <h1>Hello Danail</h1>
+//     </div>
+
+export default Hello
