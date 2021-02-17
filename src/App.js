5ffdb5780e9e7baa97b228a57f8285bbed4b4df2
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-17 13:47:07 +0200

    event handling with class component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <FunctionClick />
+        <ClassClick />
         {/* <Counter /> */}
         {/* <Message /> */}
         {/* <Greet name='Bruce'heroName='Batman'>
