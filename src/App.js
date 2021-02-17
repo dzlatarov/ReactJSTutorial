6499549b67fd1d8cab4e533732ece566daef2c3f
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-17 13:11:07 +0200

    using destructuring with functional components

--- src/App.js
+++ src/App.js
@@ -11,18 +11,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
-        {/* <Message /> */}
-        {/* <Greet name='Bruce' heroName='Batman'>
+        {/* <Counter /> */}
+        {/* <Message /> */} 
+        {/* <Greet name='Bruce'heroName='Batman'>
           <p>This is children props</p>
         </Greet>
         <Greet name='Clark' heroName='Superman'>
           <button>Action</button>
-        </Greet>
+        </Greet> */}
         <Greet name='Diana' heroName='Wonder Woman' />
-        <Welcome name='Bruce' heroName='Batman' />
-        <Welcome name='Clark' heroName='Superman' />
-        <Welcome name='Diana' heroName='Wonder Woman' /> */}
+        {/* <Welcome name='Bruce' heroName='Batman' />
+        <Welcome name='Clark' heroName='Superman' /> */}
+        {/* <Welcome name='Diana' heroName='Wonder Woman' /> */}
       </div>
     );
   }
