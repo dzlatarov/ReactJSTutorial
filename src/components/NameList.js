27cbf2e6313d23e1f25397eb672a23afb3632f85
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-18 09:51:04 +0200

    rendering list of objects

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
-    return (
-        <div>
-            {
-                names.map(name => <h2>{name}</h2>)
-            }
-        </div>
-    )
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 35,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Clark',
+            age: 31,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Diana',
+            age: 28,
+            skill: 'Vue'
+        }
+    ]
+    const personList = persons.map(person =>(
+        <Person person={person}></Person>
+    ))
+    return <div>{personList} </div>
 }
 
 export default NameList
