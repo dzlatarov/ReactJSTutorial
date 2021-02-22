fe7b095c9091c661b36b95a93b4923421a8e77ed
Danail Zlatarov <danzlatarov1@gmail.com>
2021-02-22 11:09:21 +0200

    using fragments

--- src/App.js
+++ src/App.js
@@ -18,35 +18,13 @@ import './appStyles.css'
 import styles from './appStyles.module.css'
 import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LifecycleA />
-        {/* <Form /> */}
-        {/* <h1 className='error'>Error</h1>
-        <h1 className={styles.success}>Success</h1>
-        <Inline /> */}
-        {/* <Stylesheet primary={true}/> */}
-        {/* <NameList /> */}
-        {/* <UserGreeting /> */}
-        {/* <ParentComponent /> */}
-        {/* <EventBind /> */}
-        {/* <FunctionClick />
-        <ClassClick /> */}
-        {/* <Counter /> */}
-        {/* <Message /> */}
-        {/* <Greet name='Bruce'heroName='Batman'>
-          <p>This is children props</p>
-        </Greet>
-        <Greet name='Clark' heroName='Superman'>
-          <button>Action</button>
-        </Greet> */}
-        {/* <Greet name='Diana' heroName='Wonder Woman' />
-        <Welcome name='Bruce' heroName='Batman' /> */}
-        {/* <Welcome name='Clark' heroName='Superman' /> */}
-        {/* <Welcome name='Diana' heroName='Wonder Woman' /> */}
+        <Table />
       </div>
     );
   }
